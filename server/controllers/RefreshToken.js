d66872754c3da407f7eab3fc499e3d5436f9fc06
yanamyron <115184405+yanamyron@users.noreply.github.com>
2024-06-11 21:38:03 +0300

    Update RefreshToken.js

--- server/controllers/RefreshToken.js
+++ server/controllers/RefreshToken.js
@@ -7,9 +7,7 @@ export const refreshToken = async (req, res) => {
     if (!refreshToken) return res.sendStatus(401);
     const user = await Users.findAll({
       where: {
-        refresh_token: refreshToken,
-      },
-    });
+        refresh_token: refreshToken, },});
     if (!user[0]) return res.sendStatus(403);
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
       if (err) return res.sendStatus(403);
@@ -22,6 +20,4 @@ export const refreshToken = async (req, res) => {
       res.json({ accessToken });
     });
   } catch (error) {
-    console.log(error);
-  }
-};
+    console.log(error);};
