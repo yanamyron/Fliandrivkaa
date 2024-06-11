ac27ecbd3d26c92c5bd0c95677fd6819bfe24ba9
yanamyron <115184405+yanamyron@users.noreply.github.com>
2024-06-11 21:40:39 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
+import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
 dotenv.config();
