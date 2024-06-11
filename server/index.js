8a15c4120e7c8099bb25cb2441825ae3b00668a2
yanamyron <115184405+yanamyron@users.noreply.github.com>
2024-06-11 20:43:54 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,24 +4,13 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
-
 dotenv.config();
 const app = express();
-
-// Налаштування CORS
-const corsOptions = {
-  origin: 'https://fliandrivka.netlify.app',
-  credentials: true,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ credentials: true, origin: 'https://fliandrivka.netlify.app/' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-// Перевірка підключення до бази даних
 db.authenticate()
   .then(() => {
     console.log('Database connection has been established successfully.');
