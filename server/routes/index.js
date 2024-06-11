75b13ce63f82f4458da4237effe88507f80461e3
yanamyron <115184405+yanamyron@users.noreply.github.com>
2024-06-11 19:58:25 +0300

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers, Register, Login, Logout } from "../controllers/User.js";
+import { getUsers, Register, Login/*, Logout*/ } from "../controllers/User.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 import {
